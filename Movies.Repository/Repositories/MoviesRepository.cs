86a4fd7cb6bab531357f4a1ecc6bf383bd40fa57
BenanYildiz <beni_an92@hotmail.com>
2023-01-09 02:13:20 +0300

    .

---
+++ Movies.Repository/Repositories/MoviesRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Model;
+using Movies.Core.Repositories;
+using Movies.Repository;
+using Movies.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Repository.Repositories
+{
+    public class MoviesRepository : GenericRepository<Movie>, IMoviesRepository
+    {
+        public MoviesRepository(AppDbContext context) : base(context)
+        {
+        }
+      
+    }
+}
---
+++ Movies.Service/Services/MoviesService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Movies.Core.DTOs;
+using Movies.Core.Model;
+using Movies.Core.Repositories;
+using Movies.Core.Services;
+using Movies.Core.UnitOfWorks;
+using NLayerApp.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Service.Services
+{
+    public class MoviesService : GenericService<Movie>, IMoviesService
+    {
+        private readonly IMoviesRepository _moviesRepository;
+        private readonly IMapper _mapper;
+        public MoviesService(IGenericRepository<Movie> repository,
+            IUnitOfWork unitOfWork, IMoviesRepository moviesRepository, IMapper mapper) : base(repository, unitOfWork)
+        {
+            _moviesRepository = moviesRepository;
+            _mapper = mapper;
+        }
+
+    }
+}
